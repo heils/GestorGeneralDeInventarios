c6f04ce11723f4bcebb5db2216bb2fa0587d4f7c
hmera512 <hmera@unicauca.edu.co>
2021-09-29 16:14:45 -0500

    Ajuste en las medidas

--- ProyectoZambranito/Vista/frmMenuUser.Designer.cs
+++ ProyectoZambranito/Vista/frmMenuUser.Designer.cs
@@ -48,30 +48,27 @@ namespace ProyectoZambranito.Vista
             // 
             this.btnInventario.BackgroundImage = global::ProyectoZambranito.Properties.Resources.boton;
             this.btnInventario.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.btnInventario.Location = new System.Drawing.Point(232, 38);
-            this.btnInventario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnInventario.Location = new System.Drawing.Point(174, 31);
             this.btnInventario.Name = "btnInventario";
-            this.btnInventario.Size = new System.Drawing.Size(156, 149);
+            this.btnInventario.Size = new System.Drawing.Size(144, 150);
             this.btnInventario.TabIndex = 0;
             this.btnInventario.UseVisualStyleBackColor = true;
             // 
             // btnCliente
             // 
             this.btnCliente.BackgroundImage = global::ProyectoZambranito.Properties.Resources.clientes2;
-            this.btnCliente.Location = new System.Drawing.Point(232, 243);
-            this.btnCliente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCliente.Location = new System.Drawing.Point(174, 203);
             this.btnCliente.Name = "btnCliente";
-            this.btnCliente.Size = new System.Drawing.Size(156, 149);
+            this.btnCliente.Size = new System.Drawing.Size(144, 150);
             this.btnCliente.TabIndex = 1;
             this.btnCliente.UseVisualStyleBackColor = true;
             // 
             // btnCaja
             // 
             this.btnCaja.BackgroundImage = global::ProyectoZambranito.Properties.Resources.caja;
-            this.btnCaja.Location = new System.Drawing.Point(232, 442);
-            this.btnCaja.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCaja.Location = new System.Drawing.Point(174, 413);
             this.btnCaja.Name = "btnCaja";
-            this.btnCaja.Size = new System.Drawing.Size(156, 150);
+            this.btnCaja.Size = new System.Drawing.Size(144, 154);
             this.btnCaja.TabIndex = 2;
             this.btnCaja.UseVisualStyleBackColor = true;
             this.btnCaja.Click += new System.EventHandler(this.button3_Click);
@@ -79,10 +76,9 @@ namespace ProyectoZambranito.Vista
             // ptbImagenUsuario
             // 
             this.ptbImagenUsuario.Image = global::ProyectoZambranito.Properties.Resources._90;
-            this.ptbImagenUsuario.Location = new System.Drawing.Point(4, 38);
-            this.ptbImagenUsuario.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ptbImagenUsuario.Location = new System.Drawing.Point(3, 31);
             this.ptbImagenUsuario.Name = "ptbImagenUsuario";
-            this.ptbImagenUsuario.Size = new System.Drawing.Size(115, 118);
+            this.ptbImagenUsuario.Size = new System.Drawing.Size(86, 96);
             this.ptbImagenUsuario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.ptbImagenUsuario.TabIndex = 3;
             this.ptbImagenUsuario.TabStop = false;
@@ -92,10 +88,9 @@ namespace ProyectoZambranito.Vista
             this.lblNombre.AutoSize = true;
             this.lblNombre.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombre.Location = new System.Drawing.Point(13, 219);
-            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombre.Location = new System.Drawing.Point(10, 215);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(81, 25);
+            this.lblNombre.Size = new System.Drawing.Size(65, 20);
             this.lblNombre.TabIndex = 4;
             this.lblNombre.Text = "Nombre";
             // 
@@ -104,10 +99,9 @@ namespace ProyectoZambranito.Vista
             this.lblId.AutoSize = true;
             this.lblId.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.lblId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblId.Location = new System.Drawing.Point(13, 302);
-            this.lblId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblId.Location = new System.Drawing.Point(12, 271);
             this.lblId.Name = "lblId";
-            this.lblId.Size = new System.Drawing.Size(28, 25);
+            this.lblId.Size = new System.Drawing.Size(23, 20);
             this.lblId.TabIndex = 5;
             this.lblId.Text = "Id";
             this.lblId.Click += new System.EventHandler(this.label2_Click);
@@ -117,10 +111,9 @@ namespace ProyectoZambranito.Vista
             this.lblRol.AutoSize = true;
             this.lblRol.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.lblRol.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRol.Location = new System.Drawing.Point(13, 366);
-            this.lblRol.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRol.Location = new System.Drawing.Point(12, 337);
             this.lblRol.Name = "lblRol";
-            this.lblRol.Size = new System.Drawing.Size(40, 25);
+            this.lblRol.Size = new System.Drawing.Size(33, 20);
             this.lblRol.TabIndex = 6;
             this.lblRol.Text = "Rol";
             // 
@@ -129,10 +122,9 @@ namespace ProyectoZambranito.Vista
             this.lblCargo.AutoSize = true;
             this.lblCargo.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.lblCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCargo.Location = new System.Drawing.Point(13, 442);
-            this.lblCargo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCargo.Location = new System.Drawing.Point(12, 425);
             this.lblCargo.Name = "lblCargo";
-            this.lblCargo.Size = new System.Drawing.Size(66, 25);
+            this.lblCargo.Size = new System.Drawing.Size(52, 20);
             this.lblCargo.TabIndex = 7;
             this.lblCargo.Text = "Cargo";
             // 
@@ -141,49 +133,45 @@ namespace ProyectoZambranito.Vista
             this.lblTelefono.AutoSize = true;
             this.lblTelefono.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.lblTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTelefono.Location = new System.Drawing.Point(13, 534);
-            this.lblTelefono.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTelefono.Location = new System.Drawing.Point(10, 502);
             this.lblTelefono.Name = "lblTelefono";
-            this.lblTelefono.Size = new System.Drawing.Size(89, 25);
+            this.lblTelefono.Size = new System.Drawing.Size(71, 20);
             this.lblTelefono.TabIndex = 8;
             this.lblTelefono.Text = "Telefono";
             // 
             // btnAyuda
             // 
             this.btnAyuda.BackgroundImage = global::ProyectoZambranito.Properties.Resources.botonAyuda2;
-            this.btnAyuda.Location = new System.Drawing.Point(595, 480);
-            this.btnAyuda.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAyuda.Location = new System.Drawing.Point(494, 478);
             this.btnAyuda.Name = "btnAyuda";
-            this.btnAyuda.Size = new System.Drawing.Size(163, 30);
+            this.btnAyuda.Size = new System.Drawing.Size(162, 24);
             this.btnAyuda.TabIndex = 9;
             this.btnAyuda.UseVisualStyleBackColor = true;
             // 
             // btnAcercaDe
             // 
             this.btnAcercaDe.BackgroundImage = global::ProyectoZambranito.Properties.Resources.acerca_de;
-            this.btnAcercaDe.Location = new System.Drawing.Point(595, 518);
-            this.btnAcercaDe.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAcercaDe.Location = new System.Drawing.Point(494, 523);
             this.btnAcercaDe.Name = "btnAcercaDe";
-            this.btnAcercaDe.Size = new System.Drawing.Size(163, 30);
+            this.btnAcercaDe.Size = new System.Drawing.Size(162, 24);
             this.btnAcercaDe.TabIndex = 10;
             this.btnAcercaDe.UseVisualStyleBackColor = true;
             // 
             // btnCerrarSesion
             // 
             this.btnCerrarSesion.BackgroundImage = global::ProyectoZambranito.Properties.Resources.cerrarseseion;
-            this.btnCerrarSesion.Location = new System.Drawing.Point(595, 556);
-            this.btnCerrarSesion.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCerrarSesion.Location = new System.Drawing.Point(494, 564);
             this.btnCerrarSesion.Name = "btnCerrarSesion";
-            this.btnCerrarSesion.Size = new System.Drawing.Size(163, 30);
+            this.btnCerrarSesion.Size = new System.Drawing.Size(162, 24);
             this.btnCerrarSesion.TabIndex = 11;
             this.btnCerrarSesion.UseVisualStyleBackColor = true;
             // 
             // frmMenuUser
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ProyectoZambranito.Properties.Resources.Sin_título_4;
-            this.ClientSize = new System.Drawing.Size(809, 619);
+            this.ClientSize = new System.Drawing.Size(668, 617);
             this.ControlBox = false;
             this.Controls.Add(this.btnCerrarSesion);
             this.Controls.Add(this.btnAcercaDe);
@@ -197,7 +185,6 @@ namespace ProyectoZambranito.Vista
             this.Controls.Add(this.btnCaja);
             this.Controls.Add(this.btnCliente);
             this.Controls.Add(this.btnInventario);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmMenuUser";
             this.Text = "frmMenuUser";
             ((System.ComponentModel.ISupportInitialize)(this.ptbImagenUsuario)).EndInit();
